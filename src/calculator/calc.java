4dfcea49fac20c66f37bdc08449fd4d91e5aec93
Evgeniy-Goroh <evgeniy.goroh@gmail.com>
2018-10-12 00:47:46 +0400

    add button

--- src/calculator/calc.java
+++ src/calculator/calc.java
@@ -6,15 +6,30 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class calc extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
+	String operations;
+	String answer;
+	Double firstNumber;
+	Double secondNumber;
+	
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		
+		
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -32,11 +47,201 @@ public class calc extends JFrame {
 	 */
 	public calc() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 304, 447);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 11, 270, 31);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {	
+				String backspase = null;
+				if(textField.getText().length() > 0) {
+					StringBuilder strB = new StringBuilder(textField.getText());
+					strB.deleteCharAt(textField.getText().length() - 1);
+					backspase = strB.toString();
+					textField.setText(backspase);
+				}	
+			}
+		});
+		btnNewButton.setForeground(Color.BLACK);
+		btnNewButton.setIcon(new ImageIcon(calc.class.getResource("/com/sun/javafx/scene/control/skin/caspian/images/backspace-icon.png")));
+		btnNewButton.setBounds(10, 53, 60, 60);
+		contentPane.add(btnNewButton);
+		
+		JButton btnC = new JButton("C");
+		btnC.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textField.setText(null);
+			}
+		});
+		btnC.setBounds(80, 53, 60, 60);
+		contentPane.add(btnC);
+		
+		JButton button_1 = new JButton("%");
+		button_1.setBounds(150, 53, 60, 60);
+		contentPane.add(button_1);
+		
+		JButton button_2 = new JButton("+");
+		button_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				firstNumber = Double.parseDouble(textField.getText());
+				textField.setText("");
+				operations = "+";
+			}
+		});
+		button_2.setBounds(220, 53, 60, 60);
+		contentPane.add(button_2);
+		
+		JButton btn7 = new JButton("7");
+		btn7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String EnterNumber = textField.getText() + btn7.getText();
+				textField.setText(EnterNumber);
+			}
+		});
+		btn7.setBounds(10, 124, 60, 60);
+		contentPane.add(btn7);
+		
+		JButton btn8 = new JButton("8");
+		btn8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String EnterNumber = textField.getText() + btn8.getText();
+				textField.setText(EnterNumber);
+			}
+		});
+		btn8.setBounds(80, 124, 60, 60);
+		contentPane.add(btn8);
+		
+		JButton button_5 = new JButton("-");
+		button_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				firstNumber = Double.parseDouble(textField.getText());
+				textField.setText("");
+				operations = "-";
+			}
+		});
+		button_5.setBounds(220, 124, 60, 60);
+		contentPane.add(button_5);
+		
+		JButton btn9 = new JButton("9");
+		btn9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String EnterNumber = textField.getText() + btn9.getText();
+				textField.setText(EnterNumber);
+			}
+		});
+		btn9.setBounds(150, 124, 60, 60);
+		contentPane.add(btn9);
+		
+		JButton btn4 = new JButton("4");
+		btn4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String EnterNumber = textField.getText() + btn4.getText();
+				textField.setText(EnterNumber);
+			}
+		});
+		btn4.setBounds(10, 195, 60, 60);
+		contentPane.add(btn4);
+		
+		JButton btn5 = new JButton("5");
+		btn5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String EnterNumber = textField.getText() + btn5.getText();
+				textField.setText(EnterNumber);
+			}
+		});
+		btn5.setBounds(80, 195, 60, 60);
+		contentPane.add(btn5);
+		
+		JButton btn6 = new JButton("6");
+		btn6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String EnterNumber = textField.getText() + btn6.getText();
+				textField.setText(EnterNumber);
+			}
+		});
+		btn6.setBounds(150, 195, 60, 60);
+		contentPane.add(btn6);
+		
+		JButton button_10 = new JButton("*");
+		button_10.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				firstNumber = Double.parseDouble(textField.getText());
+				textField.setText("");
+				operations = "*";
+			}
+		});
+		button_10.setBounds(220, 195, 60, 60);
+		contentPane.add(button_10);
+		
+		JButton button_11 = new JButton("/");
+		button_11.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				firstNumber = Double.parseDouble(textField.getText());
+				textField.setText("");
+				operations = "/";
+			}
+		});
+		button_11.setBounds(220, 266, 60, 60);
+		contentPane.add(button_11);
+		
+		JButton btn3 = new JButton("3");
+		btn3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String EnterNumber = textField.getText() + btn3.getText();
+				textField.setText(EnterNumber);
+			}
+		});
+		btn3.setBounds(150, 266, 60, 60);
+		contentPane.add(btn3);
+		
+		JButton btn2 = new JButton("2");
+		btn2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String EnterNumber = textField.getText() + btn2.getText();
+				textField.setText(EnterNumber);
+			}
+		});
+		btn2.setBounds(80, 266, 60, 60);
+		contentPane.add(btn2);
+		
+		JButton btn1 = new JButton("1");
+		btn1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String EnterNumber = textField.getText() + btn1.getText();
+				textField.setText(EnterNumber);
+			}
+		});
+		btn1.setBounds(10, 266, 60, 60);
+		contentPane.add(btn1);
+		
+		JButton button_15 = new JButton("=");
+		button_15.setBounds(220, 337, 60, 60);
+		contentPane.add(button_15);
+		
+		JButton button_16 = new JButton("\u00B1");
+		button_16.setBounds(150, 337, 60, 60);
+		contentPane.add(button_16);
+		
+		JButton button_17 = new JButton(".");
+		button_17.setBounds(80, 337, 60, 60);
+		contentPane.add(button_17);
+		
+		JButton btn0 = new JButton("0");
+		btn0.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String EnterNumber = textField.getText() + btn0.getText();
+				textField.setText(EnterNumber);
+			}
+		});
+		btn0.setBounds(10, 337, 60, 60);
+		contentPane.add(btn0);
 	}
-
 }
